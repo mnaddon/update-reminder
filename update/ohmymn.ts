d23972ad6fba0f49cbed52c42841895ce3beecde
ourongxing <orongxing@gmail.com>
2022-01-16 22:20:23 +0800

    release ohmymn v3.0.0

--- update/ohmymn.ts
+++ update/ohmymn.ts
@@ -1,9 +1,16 @@
 export default [
   {
     name: 'ohmymn',
+    version: '3.0.0',
+    signed: false,
+    link: 'https://busiyi.notion.site/busiyi/ohmymn-wiki-74ac16d09d17420391b8ffb0dd8cab01#b5a50f54de0941acafe35d1084fd629a',
+    time: '2022/01/16'
+  },
+  {
+    name: 'ohmymn',
     version: '3.0.0 beta4',
     signed: false,
     link: 'https://busiyi.notion.site/busiyi/ohmymn-wiki-74ac16d09d17420391b8ffb0dd8cab01#b5a50f54de0941acafe35d1084fd629a',
-    time: '2022/1/12'
+    time: '2022/01/12'
   }
 ]
